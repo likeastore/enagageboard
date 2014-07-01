c7ce455d3e4e07a8f5b4bad18cdb78af7dc0c64e
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-01 16:34:08 +0300

    analytics api access

--- source/api/searches.js
+++ source/api/searches.js
@@ -1,9 +1,43 @@
-var moment = require('moment');
-var actions = require('../db/actions');
+var request = require('request');
+var config = require('../config');
 
 function searches(app) {
-	app.route('/api/searches').get(function (req, res, next) {
+	var ensureToken = function (req, callback) {
+		if (req.session.analyticsToken) {
+			return process.nextTick(function () {
+				callback(null, req.session.analyticsToken);
+			});
+		}
 
+		var credentials = config.analytics.credentials;
+		var auth = config.analytics.url + '/auth';
+
+		request.post({url: auth, body: credentials, json: true}, function (err, response) {
+			if (err) {
+				return callback(err);
+			}
+
+			callback(null, response.body.token);
+		});
+	};
+
+	app.route('/api/searches').get(function (req, res, next) {
+		ensureToken(req, function (err, token) {
+			if (err) {
+				return next(err);
+			}
+
+			var url = config.analytics.url + '/api/reports/day/' + config.analytics.app + '?id=search&date=today';
+			var headers = {'X-Access-Token': token};
+
+			request({url: url, headers: headers, json: true}, function (err, results) {
+				if (err) {
+					return next(err);
+				}
+
+				res.json(results);
+			});
+		});
 	});
 }
 
