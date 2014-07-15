5cee3ae7c017628920867e117a16bff8628dd696
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-15 19:12:05 +0300

    reimplementing with angular-ui-router

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,39 @@
 'use strict';
 
 var app = angular.module('engageboard', [
+	'ui.router',
 	'ngRoute',
 	'ngResource',
 	'ngSanitize',
 	'highcharts-ng'
 ]);
 
-app.config(function ($routeProvider, $locationProvider) {
+app.config(function ($stateProvider, $routeProvider, $locationProvider) {
 	$routeProvider
-		.when('/', { templateUrl: 'views/dashboard.html'})
+		.when('/', { templateUrl: 'views/main.html'})
 		.otherwise({ redirectTo: '/' });
+
+	$stateProvider
+		.state('analytics', {
+			url: '/analytics',
+			template: '<h1>Analytics part</h1>'
+		})
+		.state('search', {
+			url: '/search',
+			templateUrl: 'views/dashboard.html'
+		})
+		.state('feed', {
+			url: '/feed',
+			template: '<h1>Feed part</h1>'
+		})
+		.state('queries', {
+			url: '/queries',
+			template: '<h1>Queries part</h1>'
+		})
+		.state('collections', {
+			url: '/collections',
+			template: '<h1>Collections part</h1>'
+		});
 
 	$locationProvider.html5Mode(true);
 });
