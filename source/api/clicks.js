7755fd25bebdc75775e39fc04a7dee2b4e00e58f
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-07-29 15:43:52 +0300

    additional endpoints for feed actions

--- source/api/clicks.js
+++ source/api/clicks.js
@@ -4,10 +4,8 @@ var seismo = require('seismo-client')(config.seismo.app, config.seismo.options);
 var period = require('../models/period');
 
 function clicks(app) {
-	app.route('/api/clicks').get(function (req, res, next) {
-		var query = _.extend({id: 'search-results-clicked', report: 'period'}, period.get());
-
-		seismo.report(query, function(err, results) {
+	app.route('/api/clicks/search/report').get(function (req, res, next) {
+		report('search-results-clicked', function (err, results) {
 			if (err) {
 				return next(new Error(err));
 			}
@@ -16,10 +14,8 @@ function clicks(app) {
 		});
 	});
 
-	app.route('/api/clicks/queries').get(function (req, res, next) {
-		var query = _.extend({id: 'search-results-clicked', report: 'period'}, period.get());
-
-		seismo.query(query, function(err, results) {
+	app.route('/api/clicks/search/query').get(function (req, res, next) {
+		query('search-results-clicked', function (err, results) {
 			if (err) {
 				return next(err);
 			}
@@ -36,6 +32,43 @@ function clicks(app) {
 			res.json(results);
 		});
 	});
+
+	app.route('/api/clicks/feed/report').get(function (req, res, next) {
+		report('feed-results-clicked', function (err, results) {
+			if (err) {
+				return next(new Error(err));
+			}
+
+			res.json(results);
+		});
+	});
+
+	app.route('/api/clicks/feed/query').get(function (req, res, next) {
+		query('feed-results-clicked', function (err, results) {
+			if (err) {
+				return next(err);
+			}
+
+			results = results.map(function (event) {
+				return {
+					user: event.data.user,
+					url: event.data.url,
+					timestampt: event.timestampt
+				};
+			});
+
+			res.json(results);
+		});
+	});
+
+	function report(id, callback) {
+		seismo.report(_.extend({id: id, report: 'period'}, period.get()), callback);
+	}
+
+	function query(id, callback) {
+		seismo.query(_.extend({id: id, report: 'period'}, period.get()), callback);
+	}
+
 }
 
 module.exports = clicks;
